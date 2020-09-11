a7555dd2d9adb7355bceeaabf57ffda7342b3ac4
julian <franco1231701@gmail.com>
2020-09-11 12:09:08 -0500

    commit load sliders

--- js/slider.js
+++ js/slider.js
@@ -5,6 +5,15 @@ var anterior = $("#btn-prev");
 var ant_erior = $("#ant_erior")
 var des_pues = $("#des_pues")
 var sliPerros = $("#sliPerros")
+var sliPapas = $("#sliPapas")
+var ant = $("#ant")
+var des = $("#des")
+var sliCarnes = $("#sliCarnes")
+var ante = $("#ante")
+var desp = $("#desp")
+var sliOtros = $("#sliOtros")
+var despuesd = $("#despuesd")
+var antesd = $("#antesd")
 
 
 $("#slider section:last").insertBefore("#slider section:first")
@@ -36,6 +45,7 @@ anterior.on("click",function(){
     moverI();
 });
 
+//perros
 $("#sliPerros section:last").insertBefore("#sliPerros section:first")
 
 sliPerros.css("margin-lef", "-"+100+"%");
@@ -64,4 +74,95 @@ des_pues.on("click",function(){
 ant_erior.on("click",function(){
     moverIperros();
 });
+
+//papasLocas
+$("#sliPapas section:last").insertBefore("#sliPapas section:first")
+
+sliPapas.css("margin-lef", "-"+100+"%");
+
+function moverDpapas(){
+    sliPapas.animate({
+        marginLeft:"-"+200+"%"
+    },600,function(){
+        $("#sliPapas section:first").insertAfter("#sliPapas section:last");
+        sliPapas.css("margin-lef", "-"+100+"%");
+     
+    });
+};    
+
+function moverIpapas(){
+    sliPapas.animate({
+        marginLeft:0
+    },600,function(){
+        $("#sliPapas section:last").insertBefore("#sliPapas section:first");
+        sliPapas.css("margin-lef","-"+100+"%");               
+    });
+};
+des.on("click",function(){
+    moverDpapas();
+});
+ant.on("click",function(){
+    moverIpapas();
+});
+
+//carnes
+$("#sliCarnes section:last").insertBefore("#sliCarnes section:first")
+
+sliCarnes.css("margin-lef", "-"+100+"%");
+
+function moverDcarnes(){
+    sliCarnes.animate({
+        marginLeft:"-"+200+"%"
+    },600,function(){
+        $("#sliCarnes section:first").insertAfter("#sliCarnes section:last");
+        sliCarnes.css("margin-lef", "-"+100+"%");
+     
+    });
+};    
+
+function moverIcarnes(){
+    sliCarnes.animate({
+        marginLeft:0
+    },600,function(){
+        $("#sliCarnes section:last").insertBefore("#sliCarnes section:first");
+        sliPapas.css("margin-lef","-"+100+"%");               
+    });
+};
+desp.on("click",function(){
+    moverDcarnes();
+});
+ante.on("click",function(){
+    moverIcarnes();
+});
+
+//otros
+$("#sliOtros section:last").insertBefore("#sliOtros section:first")
+
+sliOtros.css("margin-lef", "-"+100+"%");
+
+function moverDotros(){
+    sliOtros.animate({
+        marginLeft:"-"+200+"%"
+    },600,function(){
+        $("#sliOtros section:first").insertAfter("#sliOtros section:last");
+        sliOtros.css("margin-lef", "-"+100+"%");
+     
+    });
+};    
+
+function moverIotros(){
+    sliOtros.animate({
+        marginLeft:0
+    },600,function(){
+        $("#sliOtros section:last").insertBefore("#sliOtros section:first");
+        sliOtros.css("margin-lef","-"+100+"%");               
+    });
+};
+despuesd.on("click",function(){
+    moverDotros();
+});
+antesd.on("click",function(){
+    moverIotros();
+});
+
 
